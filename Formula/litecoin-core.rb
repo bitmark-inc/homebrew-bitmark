5e7b218f92e4375be4448e95f10c945340dd3c09
Jim Yeh <jim@bitmark.com>
2019-11-11 14:17:26 +0800

    Update litecoind-core version to 0.17.1

--- Formula/litecoin-core.rb
+++ Formula/litecoin-core.rb
@@ -1,8 +1,8 @@
 class LitecoinCore < Formula
   desc "A decentralized, peer to peer payment network"
   homepage "https://litecoin.org/"
-  url "https://github.com/litecoin-project/litecoin/archive/v0.16.3.tar.gz", :tag => 'v0.16.3'
-  sha256 "7788800eb4a433696a464563384cbdf83a47eebb8698325c75314171833227c8"
+  url "https://github.com/litecoin-project/litecoin/archive/v0.17.1.tar.gz", :tag => 'v0.17.1'
+  sha256 "6e05514a480990f1ff1e7c81b5443d741f4016c03ab5c1ef44bf4b169af88bee"
 
   head do
     url "https://github.com/litecoin-project/litecoin"
@@ -10,7 +10,6 @@ class LitecoinCore < Formula
 
   option "with-gui", "Build the GUI client (requires Qt5)"
 
-  depends_on "libevent"
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
@@ -18,19 +17,19 @@ class LitecoinCore < Formula
   depends_on :xcode => :build
   depends_on "berkeley-db4"
   depends_on "boost"
+  depends_on "libevent"
   depends_on "openssl"
   depends_on "miniupnpc" => :recommended
 
   if build.with? "gui"
     depends_on "qt5"
-    depends_on "protobuf@2.6"
+    depends_on "qrencode" => :recommended
     depends_on "gettext" => :recommended
   end
 
   def install
     args = ["--prefix=#{libexec}", "--disable-dependency-tracking"]
-    args << "--without-gui" if build.without? "gui"
-    args << "--without-miniupnpc" if build.without? "miniupnpc"
+    args << "--with-gui=no" if build.without? "gui"
 
     system "./autogen.sh"
     system "./configure", *args
