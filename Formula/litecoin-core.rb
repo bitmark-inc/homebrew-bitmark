82fb458f12775973c67dea6b1c89a528b5d33ae5
Jim Yeh <jim@bitmark.com>
2019-11-13 15:25:34 +0800

    Update litecoin formula to fulfill `brew audit` check

--- Formula/litecoin-core.rb
+++ Formula/litecoin-core.rb
@@ -1,7 +1,7 @@
 class LitecoinCore < Formula
-  desc "A decentralized, peer to peer payment network"
+  desc "Decentralized, peer to peer payment network"
   homepage "https://litecoin.org/"
-  url "https://github.com/litecoin-project/litecoin/archive/v0.17.1.tar.gz", :tag => 'v0.17.1'
+  url "https://github.com/litecoin-project/litecoin/archive/v0.17.1.tar.gz", :tag => "v0.17.1"
   sha256 "6e05514a480990f1ff1e7c81b5443d741f4016c03ab5c1ef44bf4b169af88bee"
 
   head do
@@ -15,7 +15,7 @@ class LitecoinCore < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on :xcode => :build
-  depends_on "berkeley-db4"
+  depends_on "berkeley-db@4"
   depends_on "boost"
   depends_on "libevent"
   depends_on "openssl"
@@ -38,5 +38,9 @@ class LitecoinCore < Formula
     system "make", "check"
     system "make", "install"
     bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/test_litecoin"
   end
 end
