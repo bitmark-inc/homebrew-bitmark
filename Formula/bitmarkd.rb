e71e4d2b9675e1129d5bf82e9041937299ef1499
Jim Yeh <jim@bitmark.com>
2019-11-13 14:46:18 +0800

    Update bitmarkd version to 0.12.2

--- Formula/bitmarkd.rb
+++ Formula/bitmarkd.rb
@@ -1,8 +1,8 @@
 class Bitmarkd < Formula
   desc "Bitmark distributed property system"
   homepage "https://github.com/bitmark-inc/bitmarkd"
-  url "https://github.com/bitmark-inc/bitmarkd/archive/v0.12.0.tar.gz"
-  sha256 "aa492d3aa4436671e14b1a5f1853159e6c644eeb3c2b9b5d878bdf80bd5cd933"
+  url "https://github.com/bitmark-inc/bitmarkd/archive/v0.12.2.tar.gz"
+  sha256 "23c3fab54a6e6ed874425f225efed41ba605766a6c66d83e184ab90dda64bce4"
   head "https://github.com/bitmark-inc/bitmarkd.git"
 
   depends_on "go" => :build
@@ -12,15 +12,13 @@ class Bitmarkd < Formula
 
   def install
     system "go", "build", "-o", ".", "-ldflags", "-X main.version=#{version}", "./..."
-    bin.install "bitmarkd", "bitmark-cli", "recorderd", "bitmark-info"
+    bin.install "bitmarkd", "bitmark-cli", "recorderd"
     prefix.install_metafiles
   end
 
   test do
-    assert_match "bitmarkd: version: 0.12.0", shell_output("#{bin}/bitmarkd --version 2>&1", 1)
-    assert_match "recorderd: version: 0.12.0", shell_output("#{bin}/recorderd --version 2>&1", 1)
-    assert_match "bitmark-info: version: 0.12.0", shell_output("#{bin}/bitmark-info --version 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/bitmarkd version")
+    assert_match version.to_s, shell_output("#{bin}/recorderd version")
     assert_match version.to_s, shell_output("#{bin}/bitmark-cli version")
   end
 end
-
